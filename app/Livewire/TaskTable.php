7405c3870325ad901b51acd5aa96b18f32ab6f0c
salman rajabi <77535863+dastanrab@users.noreply.github.com>
2024-08-20 23:38:03 +0330

    remove unuse functions

--- app/Livewire/TaskTable.php
+++ app/Livewire/TaskTable.php
@@ -21,55 +21,7 @@ class TaskTable extends Component
         $this->tasks=Task::query()->get()->toArray();
     }
 
-    public function fetchTasks()
-    {
-            $response = Http::get('localhost/api/task');
-            $this->tasks = $response->json();
-    }
 
-    public function selectTask($taskId)
-    {
-        dd('sss');
-        $task = collect($this->tasks)->firstWhere('id', $taskId);
-        $this->selectedTaskId = $task['id'];
-        $this->taskDescription = $task['description'];
-        $this->taskStatus = $task['status'];
-    }
-
-    public function updateTask()
-    {
-        if ($this->selectedTaskId) {
-            try {
-                Http::put("https://your-api-url.com/api/tasks/{$this->selectedTaskId}", [
-                    'description' => $this->taskDescription,
-                    'status' => $this->taskStatus,
-                ]);
-
-                $this->fetchTasks();
-                $this->resetInputFields();
-            } catch (\Exception $e) {
-                Log::error('Error updating task: ' . $e->getMessage());
-            }
-        }
-    }
-
-    public function taskUpdated($taskData)
-    {
-        // Update the task in the local list
-        $this->tasks = collect($this->tasks)->map(function ($task) use ($taskData) {
-            if ($task['id'] === $taskData['id']) {
-                return $taskData;
-            }
-            return $task;
-        })->toArray();
-    }
-
-    private function resetInputFields()
-    {
-        $this->taskDescription = '';
-        $this->taskStatus = '';
-        $this->selectedTaskId = null;
-    }
 
     public function render()
     {
